xrobau/pve-manager

commit 8f60ee4ccacc14f1bbe025874522c151cc375f83

ui: add storage plugin base class
This removes *a lot* of code duplication.

I add a base class for the storage edit window and for its containing
input panel, they implement the shared stuff. Especially the window
was mostly a 1:1 copy...

I look hard for a way to split up this patch, but I did not really
found one which would not generate a lot of work for no value added
(value being 'revertability' and better git history here).
nd actually not too much happens, the same thing happens just over
and over again.
Thus, I've thrown in the dynamic creation of the storage add menu
items here too.

I remove all storage specific Edit windows, they where all just >95%
duplicates of each other.
Special functionallity, i.e. some data deletion/transforming before
submitting gets done with onGetValues.

For the RBD external vs PVE plugin I just added a minimal child class
to RBD which only tells it'S parent that it is the pve one, this is
nice for the mapping and should be easy to understand when reading
the code.

Tried to test an add and an edit of all visible storage plugins,
seems to be OK now.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/storage/IScsiEdit.js
@@ -57,39 +57,26 @@ Ext.define('PVE.storage.IScsiScan', {
 });
 
 Ext.define('PVE.storage.IScsiInputPanel', {
-    extend: 'Proxmox.panel.InputPanel',
+    extend: 'PVE.panel.StorageBase',
 
     onGetValues: function(values) {
 	var me = this;
 
-	if (me.isCreate) {
-	    values.type = 'iscsi';
-	} else {
-	    delete values.storage;
-	}
-
 	values.content = values.luns ? 'images' : 'none';
 	delete values.luns;
 
-	values.disable = values.enable ? 0 : 1;
-	delete values.enable;
+	me.callParent([values]);
+    },
 
-	return values;
+    setValues: function(values) {
+	values.luns = (values.content === 'images') ? true : false;
+	this.callParent();
     },
 
     initComponent : function() {
 	var me = this;
 
-
 	me.column1 = [
-	    {
-		xtype: me.isCreate ? 'textfield' : 'displayfield',
-		name: 'storage',
-		value: me.storageId || '',
-		fieldLabel: 'ID',
-		vtype: 'StorageId',
-		allowBlank: false
-	    },
 	    {
 		xtype: me.isCreate ? 'textfield' : 'displayfield',
 		name: 'portal',
@@ -118,22 +105,6 @@ Ext.define('PVE.storage.IScsiInputPanel', {
 
 	me.column2 = [
 	    {
-		xtype: 'pveNodeSelector',
-		name: 'nodes',
-		fieldLabel: gettext('Nodes'),
-		emptyText: gettext('All') + ' (' +
-		    gettext('No restrictions') +')',
-		multiSelect: true,
-		autoSelect: false
-	    },
-	    {
-		xtype: 'proxmoxcheckbox',
-		name: 'enable',
-		checked: true,
-		uncheckedValue: 0,
-		fieldLabel: gettext('Enable')
-	    },
-	    {
 		xtype: 'checkbox',
 		name: 'luns',
 		checked: true,
@@ -142,56 +113,5 @@ Ext.define('PVE.storage.IScsiInputPanel', {
 	];
 
 	me.callParent();
-    }
-});
-
-Ext.define('PVE.storage.IScsiEdit', {
-    extend: 'Proxmox.window.Edit',
-
-    initComponent : function() {
-	var me = this;
-
-	me.isCreate = !me.storageId;
-
-	if (me.isCreate) {
-            me.url = '/api2/extjs/storage';
-            me.method = 'POST';
-        } else {
-            me.url = '/api2/extjs/storage/' + me.storageId;
-            me.method = 'PUT';
-        }
-
-	var ipanel = Ext.create('PVE.storage.IScsiInputPanel', {
-	    isCreate: me.isCreate,
-	    storageId: me.storageId
-	});
-
-	Ext.apply(me, {
-            subject: PVE.Utils.format_storage_type('iscsi'),
-	    isAdd: true,
-	    items: [ ipanel ]
-	});
-
-	me.callParent();
-
-	if (!me.isCreate) {
-	    me.load({
-		success:  function(response, options) {
-		    var values = response.result.data;
-		    var ctypes = values.content || '';
-
-		    if (values.storage === 'local') {
-			values.content = ctypes.split(',');
-		    }
-		    if (values.nodes) {
-			values.nodes = values.nodes.split(',');
-		    }
-		    values.enable = values.disable ? 0 : 1;
-		    values.luns = (values.content === 'images') ? true : false;
-
-		    ipanel.setValues(values);
-		}
-	    });
-	}
     }
 });

www/manager6/Makefile
@@ -148,6 +148,7 @@ JSSRC= 				                 	\
 	pool/StatusView.js				\
 	pool/Summary.js					\
 	pool/Config.js					\
+	storage/Base.js					\
 	storage/ContentView.js				\
 	storage/StatusView.js				\
 	storage/Summary.js				\

www/manager6/dc/StorageView.js
@@ -1,4 +1,3 @@
-
 Ext.define('PVE.dc.StorageView', {
     extend: 'Ext.grid.GridPanel',
 
@@ -8,6 +7,23 @@ Ext.define('PVE.dc.StorageView', {
 
     stateful: true,
     stateId: 'grid-dc-storage',
+
+    createStorageEditWindow: function(type, sid) {
+	var schema = PVE.Utils.storageSchema[type];
+	if (!schema || !schema.ipanel) {
+	    throw "no editor registered for storage type: " + type;
+	}
+
+	Ext.create('PVE.storage.BaseEdit', {
+	    paneltype: 'PVE.storage.' + schema.ipanel,
+	    type: type,
+	    storageId: sid,
+	    autoShow: true,
+	    listeners: {
+		destroy: this.reloadStore
+	    }
+	});
+    },
 
     initComponent : function() {
 	var me = this;
@@ -35,41 +51,14 @@ Ext.define('PVE.dc.StorageView', {
 	    if (!rec) {
 		return;
 	    }
-	    var type = rec.data.type;
+	    var type = rec.data.type,
+	        sid = rec.data.storage;
 
-	    var editor;
-	    if (type === 'dir') {
-		editor = 'PVE.storage.DirEdit';
-	    } else if (type === 'nfs') {
-		editor = 'PVE.storage.NFSEdit';
-	    } else if (type === 'cifs') {
-		editor = 'PVE.storage.CIFSEdit';
-	    } else if (type === 'glusterfs') {
-		editor = 'PVE.storage.GlusterFsEdit';
-	    } else if (type === 'lvm') {
-		editor = 'PVE.storage.LVMEdit';
-	    } else if (type === 'lvmthin') {
-		editor = 'PVE.storage.LvmThinEdit';
-	    } else if (type === 'iscsi') {
-		editor = 'PVE.storage.IScsiEdit';
-	    } else if (type === 'rbd') {
-		editor = 'PVE.storage.RBDEdit';
-	    } else if (type === 'sheepdog') {
-		editor = 'PVE.storage.SheepdogEdit';
-	    } else if (type === 'zfs') {
-		editor = 'PVE.storage.ZFSEdit';
-	    } else if (type === 'zfspool') {
-		editor = 'PVE.storage.ZFSPoolEdit';
-	    } else {
-		return;
+	    if (type === 'rbd' && !rec.data.monhost) {
+		type = 'pveceph';
 	    }
-	    var win = Ext.create(editor, {
-		storageId: rec.data.storage,
-		pveceph: !rec.data.monhost
-	    });
 
-	    win.show();
-	    win.on('destroy', reload);
+	    me.createStorageEditWindow(type, sid);
 	};
 
 	var edit_btn = new Proxmox.button.Button({
@@ -82,13 +71,30 @@ Ext.define('PVE.dc.StorageView', {
 	var remove_btn = Ext.create('Proxmox.button.StdRemoveButton', {
 	    selModel: sm,
 	    baseurl: '/storage/',
-	    callback: function() {
-		reload();
-	    }
+	    callback: reload
 	});
+
+	// else we cannot dynamically generate the add menu handlers
+	var addHandleGenerator = function(type) {
+	    return function() { me.createStorageEditWindow(type); };
+	};
+	var addMenuItems = [], type;
+	/*jslint forin: true */
+	for (type in PVE.Utils.storageSchema) {
+	    var storage = PVE.Utils.storageSchema[type];
+	    if (storage.hideAdd) {
+		continue;
+	    }
+	    addMenuItems.push({
+		text:  PVE.Utils.format_storage_type(type),
+		iconCls: 'fa fa-fw fa-' + storage.faIcon,
+		handler: addHandleGenerator(type)
+	    });
+	}
 
 	Ext.apply(me, {
 	    store: store,
+	    reloadStore: reload,
 	    selModel: sm,
 	    viewConfig: {
 		trackOver: false
@@ -97,123 +103,7 @@ Ext.define('PVE.dc.StorageView', {
 		{
 		    text: gettext('Add'),
 		    menu: new Ext.menu.Menu({
-			items: [
-			    {
-				text:  PVE.Utils.format_storage_type('dir'),
-				iconCls: 'fa fa-fw fa-folder',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.DirEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-
-			    },
-			    {
-				text:  PVE.Utils.format_storage_type('lvm'),
-				iconCls: 'fa fa-fw fa-folder',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.LVMEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text:  PVE.Utils.format_storage_type('lvmthin'),
-				iconCls: 'fa fa-fw fa-folder',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.LvmThinEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text:  PVE.Utils.format_storage_type('nfs'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.NFSEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text:  PVE.Utils.format_storage_type('cifs'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.CIFSEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text: PVE.Utils.format_storage_type('iscsi'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.IScsiEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text: PVE.Utils.format_storage_type('glusterfs'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.GlusterFsEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text: PVE.Utils.format_storage_type('pveceph'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.RBDEdit', {
-					pveceph: 1
-				    });
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text: PVE.Utils.format_storage_type('rbd_ext'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.RBDEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-				text: PVE.Utils.format_storage_type('zfs'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.ZFSEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    },
-			    {
-                                text: PVE.Utils.format_storage_type('zfspool'),
-                                iconCls: 'fa fa-fw fa-folder',
-                                handler: function() {
-                                    var win = Ext.create('PVE.storage.ZFSPoolEdit', {});
-                                    win.on('destroy', reload);
-                                    win.show();
-                                }
-                            }
-
-/* the following type are conidered unstable
- * so we do not enable that on the GUI for now
-			    {
-				text: PVE.Utils.format_storage_type('sheepdog'),
-				iconCls: 'fa fa-fw fa-building',
-				handler: function() {
-				    var win = Ext.create('PVE.storage.SheepdogEdit', {});
-				    win.on('destroy', reload);
-				    win.show();
-				}
-			    }
-*/
-			]
+			items: addMenuItems
 		    })
 		},
 		remove_btn,

www/manager6/storage/Base.js
@@ -1,0 +1,105 @@
+Ext.define('PVE.panel.StorageBase', {
+    extend: 'Proxmox.panel.InputPanel',
+    controller: 'storageEdit',
+
+    type: '',
+
+    onGetValues: function(values) {
+	var me = this;
+
+	if (me.isCreate) {
+	    values.type = me.type;
+	} else {
+	    delete values.storage;
+	}
+
+	values.disable = values.enable ? 0 : 1;
+	delete values.enable;
+
+	return values;
+    },
+
+    initComponent : function() {
+	var me = this;
+
+	me.column1.unshift({
+	    xtype: me.isCreate ? 'textfield' : 'displayfield',
+	    name: 'storage',
+	    value: me.storageId || '',
+	    fieldLabel: 'ID',
+	    vtype: 'StorageId',
+	    allowBlank: false
+	});
+
+	me.column2.unshift(
+	    {
+		xtype: 'pveNodeSelector',
+		name: 'nodes',
+		disabled: me.storageId === 'local',
+		fieldLabel: gettext('Nodes'),
+		emptyText: gettext('All') + ' (' + gettext('No restrictions') +')',
+		multiSelect: true,
+		autoSelect: false
+	    },
+	    {
+		xtype: 'proxmoxcheckbox',
+		name: 'enable',
+		checked: true,
+		uncheckedValue: 0,
+		fieldLabel: gettext('Enable')
+	    }
+	);
+
+	me.callParent();
+    }
+});
+
+Ext.define('PVE.storage.BaseEdit', {
+    extend: 'Proxmox.window.Edit',
+
+    initComponent : function() {
+	var me = this;
+
+	me.isCreate = !me.storageId;
+
+	if (me.isCreate) {
+            me.url = '/api2/extjs/storage';
+            me.method = 'POST';
+        } else {
+            me.url = '/api2/extjs/storage/' + me.storageId;
+            me.method = 'PUT';
+        }
+
+	var ipanel = Ext.create(me.paneltype, {
+	    type: me.type,
+	    isCreate: me.isCreate,
+	    storageId: me.storageId
+	});
+
+	Ext.apply(me, {
+            subject: PVE.Utils.format_storage_type(me.type),
+	    isAdd: true,
+	    items: [ ipanel ]
+	});
+
+	me.callParent();
+
+	if (!me.isCreate) {
+	    me.load({
+		success:  function(response, options) {
+		    var values = response.result.data;
+		    var ctypes = values.content || '';
+
+		    values.content = ctypes.split(',');
+
+		    if (values.nodes) {
+			values.nodes = values.nodes.split(',');
+		    }
+		    values.enable = values.disable ? 0 : 1;
+
+		    ipanel.setValues(values);
+		}
+	    });
+	}
+    }
+});

www/manager6/storage/CIFSEdit.js
@@ -86,23 +86,7 @@ Ext.define('PVE.storage.CIFSScan', {
 });
 
 Ext.define('PVE.storage.CIFSInputPanel', {
-    extend: 'Proxmox.panel.InputPanel',
-    controller: 'storageEdit',
-
-    onGetValues: function(values) {
-	var me = this;
-
-	if (me.isCreate) {
-	    values.type = 'cifs';
-	} else {
-	    delete values.storage;
-	}
-
-	values.disable = values.enable ? 0 : 1;
-	delete values.enable;
-
-	return values;
-    },
+    extend: 'PVE.panel.StorageBase',
 
     initComponent : function() {
 	var me = this;
@@ -126,14 +110,6 @@ Ext.define('PVE.storage.CIFSInputPanel', {
 	});
 
 	me.column1 = [
-	    {
-		xtype: me.isCreate ? 'textfield' : 'displayfield',
-		name: 'storage',
-		value: me.storageId || '',
-		fieldLabel: 'ID',
-		vtype: 'StorageId',
-		allowBlank: false
-	    },
 	    {
 		xtype: me.isCreate ? 'textfield' : 'displayfield',
 		name: 'server',
@@ -190,22 +166,6 @@ Ext.define('PVE.storage.CIFSInputPanel', {
 
 	me.column2 = [
 	    {
-		xtype: 'pveNodeSelector',
-		name: 'nodes',
-		fieldLabel: gettext('Nodes'),
-		emptyText: gettext('All') + ' (' +
-		    gettext('No restrictions') +')',
-		multiSelect: true,
-		autoSelect: false
-	    },
-	    {
-		xtype: 'proxmoxcheckbox',
-		name: 'enable',
-		checked: true,
-		uncheckedValue: 0,
-		fieldLabel: gettext('Enable')
-	    },
-	    {
 		xtype: 'proxmoxintegerfield',
 		fieldLabel: gettext('Max Backups'),
 		name: 'maxfiles',
@@ -235,53 +195,5 @@ Ext.define('PVE.storage.CIFSInputPanel', {
 	];
 
 	me.callParent();
-    }
-});
-
-Ext.define('PVE.storage.CIFSEdit', {
-    extend: 'Proxmox.window.Edit',
-
-    initComponent : function() {
-	var me = this;
-
-	me.isCreate = !me.storageId;
-
-	if (me.isCreate) {
-            me.url = '/api2/extjs/storage';
-            me.method = 'POST';
-        } else {
-            me.url = '/api2/extjs/storage/' + me.storageId;
-            me.method = 'PUT';
-        }
-
-	var ipanel = Ext.create('PVE.storage.CIFSInputPanel', {
-	    isCreate: me.isCreate,
-	    storageId: me.storageId
-	});
-
-	Ext.apply(me, {
-            subject: 'CIFS',
-	    isAdd: true,
-	    items: [ ipanel ]
-	});
-
-	me.callParent();
-
-	if (!me.isCreate) {
-	    me.load({
-		success:  function(response, options) {
-		    var values = response.result.data;
-		    var ctypes = values.content || '';
-
-		    values.content = ctypes.split(',');
-
-		    if (values.nodes) {
-			values.nodes = values.nodes.split(',');
-		    }
-		    values.enable = values.disable ? 0 : 1;
-		    ipanel.setValues(values);
-		}
-	    });
-	}
     }
 });

www/manager6/storage/DirEdit.js
@@ -1,35 +1,10 @@
 Ext.define('PVE.storage.DirInputPanel', {
-    extend: 'Proxmox.panel.InputPanel',
-    controller: 'storageEdit',
-
-    onGetValues: function(values) {
-	var me = this;
-
-	if (me.isCreate) {
-	    values.type = 'dir';
-	} else {
-	    delete values.storage;
-	}
-
-	values.disable = values.enable ? 0 : 1;
-	delete values.enable;
-
-	return values;
-    },
+    extend: 'PVE.panel.StorageBase',
 
     initComponent : function() {
 	var me = this;
 
-
 	me.column1 = [
-	    {
-		xtype: me.isCreate ? 'textfield' : 'displayfield',
-		name: 'storage',
-		value: me.storageId || '',
-		fieldLabel: 'ID',
-		vtype: 'StorageId',
-		allowBlank: false
-	    },
 	    {
 		xtype: me.isCreate ? 'textfield' : 'displayfield',
 		name: 'path',
@@ -50,13 +25,6 @@ Ext.define('PVE.storage.DirInputPanel', {
 	me.column2 = [
 	    {
 		xtype: 'proxmoxcheckbox',
-		name: 'enable',
-		checked: true,
-		uncheckedValue: 0,
-		fieldLabel: gettext('Enable')
-	    },
-	    {
-		xtype: 'proxmoxcheckbox',
 		name: 'shared',
 		uncheckedValue: 0,
 		fieldLabel: gettext('Shared')
@@ -74,67 +42,6 @@ Ext.define('PVE.storage.DirInputPanel', {
 	    }
 	];
 
-	if (me.isCreate || me.storageId !== 'local') {
-	    me.column2.unshift({
-		xtype: 'pveNodeSelector',
-		name: 'nodes',
-		fieldLabel: gettext('Nodes'),
-		emptyText: gettext('All') + ' (' +
-		    gettext('No restrictions') +')',
-		multiSelect: true,
-		autoSelect: false
-	    });
-	}
-
 	me.callParent();
-    }
-});
-
-Ext.define('PVE.storage.DirEdit', {
-    extend: 'Proxmox.window.Edit',
-
-    initComponent : function() {
-	var me = this;
-
-	me.isCreate = !me.storageId;
-
-	if (me.isCreate) {
-            me.url = '/api2/extjs/storage';
-            me.method = 'POST';
-        } else {
-            me.url = '/api2/extjs/storage/' + me.storageId;
-            me.method = 'PUT';
-        }
-
-	var ipanel = Ext.create('PVE.storage.DirInputPanel', {
-	    isCreate: me.isCreate,
-	    storageId: me.storageId
-	});
-
-	Ext.apply(me, {
-            subject: PVE.Utils.format_storage_type('dir'),
-	    isAdd: true,
-	    items: [ ipanel ]
-	});
-
-	me.callParent();
-
-	if (!me.isCreate) {
-	    me.load({
-		success:  function(response, options) {
-		    var values = response.result.data;
-		    var ctypes = values.content || '';
-
-		    values.content = ctypes.split(',');
-
-		    if (values.nodes) {
-			values.nodes = values.nodes.split(',');
-		    }
-		    values.enable = values.disable ? 0 : 1;
-
-		    ipanel.setValues(values);
-		}
-	    });
-	}
     }
 });

www/manager6/storage/GlusterFsEdit.js
@@ -58,37 +58,12 @@ Ext.define('PVE.storage.GlusterFsScan', {
 });
 
 Ext.define('PVE.storage.GlusterFsInputPanel', {
-    extend: 'Proxmox.panel.InputPanel',
-    controller: 'storageEdit',
-
-    onGetValues: function(values) {
-	var me = this;
-
-	if (me.isCreate) {
-	    values.type = 'glusterfs';
-	} else {
-	    delete values.storage;
-	}
-
-	values.disable = values.enable ? 0 : 1;
-	delete values.enable;
-
-	return values;
-    },
+    extend: 'PVE.panel.StorageBase',
 
     initComponent : function() {
 	var me = this;
 
-
 	me.column1 = [
-	    {
-		xtype: me.isCreate ? 'textfield' : 'displayfield',
-		name: 'storage',
-		value: me.storageId || '',
-		fieldLabel: 'ID',
-		vtype: 'StorageId',
-		allowBlank: false
-	    },
 	    {
 		xtype: me.isCreate ? 'textfield' : 'displayfield',
 		name: 'server',
@@ -132,22 +107,6 @@ Ext.define('PVE.storage.GlusterFsInputPanel', {
 
 	me.column2 = [
 	    {
-		xtype: 'pveNodeSelector',
-		name: 'nodes',
-		fieldLabel: gettext('Nodes'),
-		emptyText: gettext('All') + ' (' +
-		    gettext('No restrictions') +')',
-		multiSelect: true,
-		autoSelect: false
-	    },
-	    {
-		xtype: 'proxmoxcheckbox',
-		name: 'enable',
-		checked: true,
-		uncheckedValue: 0,
-		fieldLabel: gettext('Enable')
-	    },
-	    {
 		xtype: 'proxmoxintegerfield',
 		fieldLabel: gettext('Max Backups'),
 		disabled: true,
@@ -161,53 +120,5 @@ Ext.define('PVE.storage.GlusterFsInputPanel', {
 	];
 
 	me.callParent();
-    }
-});
-
-Ext.define('PVE.storage.GlusterFsEdit', {
-    extend: 'Proxmox.window.Edit',
-
-    initComponent : function() {
-	var me = this;
-
-	me.isCreate = !me.storageId;
-
-	if (me.isCreate) {
-            me.url = '/api2/extjs/storage';
-            me.method = 'POST';
-        } else {
-            me.url = '/api2/extjs/storage/' + me.storageId;
-            me.method = 'PUT';
-        }
-
-	var ipanel = Ext.create('PVE.storage.GlusterFsInputPanel', {
-	    isCreate: me.isCreate,
-	    storageId: me.storageId
-	});
-
-	Ext.apply(me, {
-            subject: PVE.Utils.format_storage_type('glusterfs'),
-	    isAdd: true,
-	    items: [ ipanel ]
-	});
-
-	me.callParent();
-
-	if (!me.isCreate) {
-	    me.load({
-		success:  function(response, options) {
-		    var values = response.result.data;
-		    var ctypes = values.content || '';
-
-		    values.content = ctypes.split(',');
-
-		    if (values.nodes) {
-			values.nodes = values.nodes.split(',');
-		    }
-		    values.enable = values.disable ? 0 : 1;
-		    ipanel.setValues(values);
-		}
-	    });
-	}
     }
 });

www/manager6/storage/LVMEdit.js
@@ -84,37 +84,12 @@ Ext.define('PVE.storage.BaseStorageSelector', {
 });
 
 Ext.define('PVE.storage.LVMInputPanel', {
-    extend: 'Proxmox.panel.InputPanel',
-
-    onGetValues: function(values) {
-	var me = this;
-
-	if (me.isCreate) {
-	    values.type = 'lvm';
-	} else {
-	    delete values.storage;
-	}
-
-	values.disable = values.enable ? 0 : 1;
-	delete values.enable;
-
-	return values;
-    },
+    extend: 'PVE.panel.StorageBase',
 
     initComponent : function() {
 	var me = this;
 
-	me.column1 = [
-	    {
-		xtype: me.isCreate ? 'textfield' : 'displayfield',
-		name: 'storage',
-		value: me.storageId || '',
-		fieldLabel: 'ID',
-		vtype: 'StorageId',
-		submitValue: !!me.isCreate,
-		allowBlank: false
-	    }
-	];
+	me.column1 = [];
 
 	var vgnameField = Ext.createWidget(me.isCreate ? 'textfield' : 'displayfield', {
 	    name: 'vgname',
@@ -192,22 +167,6 @@ Ext.define('PVE.storage.LVMInputPanel', {
 
 	me.column2 = [
 	    {
-		xtype: 'pveNodeSelector',
-		name: 'nodes',
-		fieldLabel: gettext('Nodes'),
-		emptyText: gettext('All') + ' (' +
-		    gettext('No restrictions') +')',
-		multiSelect: true,
-		autoSelect: false
-	    },
-	    {
-		xtype: 'proxmoxcheckbox',
-		name: 'enable',
-		checked: true,
-		uncheckedValue: 0,
-		fieldLabel: gettext('Enable')
-	    },
-	    {
 		xtype: 'proxmoxcheckbox',
 		name: 'shared',
 		uncheckedValue: 0,
@@ -216,53 +175,5 @@ Ext.define('PVE.storage.LVMInputPanel', {
 	];
 
 	me.callParent();
-    }
-});
-
-Ext.define('PVE.storage.LVMEdit', {
-    extend: 'Proxmox.window.Edit',
-
-    initComponent : function() {
-	var me = this;
-
-	me.isCreate = !me.storageId;
-
-	if (me.isCreate) {
-            me.url = '/api2/extjs/storage';
-            me.method = 'POST';
-        } else {
-            me.url = '/api2/extjs/storage/' + me.storageId;
-            me.method = 'PUT';
-        }
-
-	var ipanel = Ext.create('PVE.storage.LVMInputPanel', {
-	    isCreate: me.isCreate,
-	    storageId: me.storageId
-	});
-
-	Ext.apply(me, {
-            subject: PVE.Utils.format_storage_type('lvm'),
-	    isAdd: true,
-	    items: [ ipanel ]
-	});
-
-	me.callParent();
-
-	if (!me.isCreate) {
-	    me.load({
-		success:  function(response, options) {
-		    var values = response.result.data;
-		    var ctypes = values.content || '';
-
-		    values.content = ctypes.split(',');
-
-		    if (values.nodes) {
-			values.nodes = values.nodes.split(',');
-		    }
-		    values.enable = values.disable ? 0 : 1;
-		    ipanel.setValues(values);
-		}
-	    });
-	}
     }
 });

commit 6348ef44ecb353fe5299cdc56665d692e2bdaefa

ui: dc/storage: allow to renenable iscsi storages again
The dc/storage refactoring done in
commit 8f60ee4ccacc14f1bbe025874522c151cc375f83
introduced a regression where an once deactivated iSCSI storage could
not be activated again through UI, as we did not called the parent
setValue function correctly, thus the Enable checkbox was always set
to true. A uncheck -> check cycle couldn't also be done as the dirty
changes check disabled the submit button in this case.

Enable/Disable through API or CLI still worked.

Reported-by: Bastian Sebode <b.sebode@linet-services.de>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/storage/IScsiEdit.js
@@ -70,7 +70,7 @@ Ext.define('PVE.storage.IScsiInputPanel', {
 
     setValues: function(values) {
 	values.luns = (values.content === 'images') ? true : false;
-	this.callParent();
+	this.callParent([values]);
     },
 
     initComponent : function() {