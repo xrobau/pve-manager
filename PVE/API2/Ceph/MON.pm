xrobau/pve-manager

commit 86ed64f919352391d85fb2dacd62415b2d58eb3f

api: ceph: mon: fix handling of IPv6 addresses in assert_mon_prerequisites
by comparing their canonical forms.

Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/API2/Ceph/MON.pm
@@ -89,14 +89,25 @@ my $ips_from_mon_host = sub {
 my $assert_mon_prerequisites = sub {
     my ($cfg, $monhash, $monid, $monip) = @_;
 
-    my $ip_regex = '(?:^|[^\d])' . # start or nondigit
-	       "\Q$monip\E" .  # the ip not interpreted as regex
-	       '(?:[^\d]|$)';  # end or nondigit
+    $monip = PVE::Network::canonical_ip($monip);
 
-    if (($cfg->{global}->{mon_host} && $cfg->{global}->{mon_host} =~ m/$ip_regex/) ||
-	grep { $_->{addr} && $_->{addr}  =~ m/$ip_regex/ } values %$monhash) {
-	die "monitor address '$monip' already in use\n";
+    my $used_ips = {};
+
+    my $mon_host_ips = $ips_from_mon_host->($cfg->{global}->{mon_host});
+
+    for my $mon_host_ip (@{$mon_host_ips}) {
+	my $ip = PVE::Network::canonical_ip($mon_host_ip);
+	$used_ips->{$ip} = 1;
     }
+
+    for my $mon (values %{$monhash}) {
+	next if !defined($mon->{addr});
+
+	my $ip = PVE::Network::canonical_ip($mon->{addr});
+	$used_ips->{$ip} = 1;
+    }
+
+    die "monitor address '$monip' already in use\n" if $used_ips->{$monip};
 
     if (defined($monhash->{$monid})) {
 	die "monitor '$monid' already exists\n";

commit 61ad3f4d99745b78ad9a47dfd20286336baf3a00

api: ceph: mon: make checking for duplicate addresses more robust
Because $mon->{addr} might come with a port attached (affects monitors
created with PVE 5.4 as reported in the community forum [0]), or even
be a hostname (according to the code in Ceph/Services.pm). Although
the latter shouldn't happen for configurations created by PVE.

[0]: https://forum.proxmox.com/threads/105904/

Fixes: 86ed64f9 ("api: ceph: mon: fix handling of IPv6 addresses in assert_mon_prerequisites")
Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/API2/Ceph/MON.pm
@@ -132,8 +132,10 @@ my $assert_mon_prerequisites = sub {
     for my $mon (values %{$monhash}) {
 	next if !defined($mon->{addr});
 
-	my $ip = PVE::Network::canonical_ip($mon->{addr});
-	$used_ips->{$ip} = 1;
+	for my $ip ($ips_from_mon_host->($mon->{addr})->@*) {
+	    $ip = PVE::Network::canonical_ip($ip);
+	    $used_ips->{$ip} = 1;
+	}
     }
 
     for my $monip (@{$monips}) {