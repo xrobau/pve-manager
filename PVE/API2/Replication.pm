xrobau/pve-manager

commit e6b8af20a6fbfc8e2ae79ae53f8ee47b3fe2746a

replication: sent always mail for first three tries and move helper
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Replication.pm
@@ -70,22 +70,23 @@ sub run_single_job {
     die $@ if $@;
 }
 
+
+my sub _should_mail_at_failcount {
+    my ($fail_count) = @_;
+
+    return 1 if $fail_count <= 3; # always send the first few for better visibility of the issue
+
+    # failing job is re-tried every half hour, try to send one mail after 1, 2, 4, 8, etc. days
+    my $i = 1;
+    while ($i * 48 < $fail_count) {
+	$i = $i * 2;
+    }
+    return $i * 48 == $fail_count;
+};
+
 # passing $now and $verbose is useful for regression testing
 sub run_jobs {
     my ($now, $logfunc, $verbose, $mail) = @_;
-
-    my $mail_at_fail_count = sub {
-	my ($fail_count) = @_;
-
-	return 1 if $fail_count == 1;
-
-	# failing job is re-tried every half hour, try to send one mail after 1, 2, 4, 8, etc. days
-	my $i = 1;
-	while ($i * 48 < $fail_count) {
-	    $i = $i * 2;
-	}
-	return $i * 48 == $fail_count;
-    };
 
     my $iteration = $now // time();
 
@@ -106,7 +107,7 @@ sub run_jobs {
 		my $jobstate = PVE::ReplicationState::extract_job_state($state, $jobcfg);
 		eval {
 		    PVE::Tools::sendmail('root', "Replication Job: $jobcfg->{id} failed", $err)
-			if $mail && $mail_at_fail_count->($jobstate->{fail_count});
+			if $mail && _should_mail_at_failcount($jobstate->{fail_count});
 		};
 		warn ": $@" if $@;
 	    }

commit 32f19c1456f08ffe4d02a3593106eb659d7c1fde

fix #4111: replication: don't send mail when fail count is zero
which can happen when failing to obtain the guest's migration lock.
This led to a lot of mails being sent during migration (timeout for
obtaining lock is only 2 seconds and we run it in a loop).

One could argue that obtaining the lock should increase the fail
count, but without the lock, the job state should not be touched and
even the first three mails upon migration could be considered spam.

Fixes: e6b8af20 ("replication: sent always mail for first three tries and move helper")
Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/API2/Replication.pm
@@ -77,6 +77,10 @@ sub run_single_job {
 my sub _should_mail_at_failcount {
     my ($fail_count) = @_;
 
+    # avoid spam during migration (bug #4111): when failing to obtain the guest's migration lock,
+    # fail_count will be 0
+    return 0 if $fail_count == 0;
+
     return 1 if $fail_count <= 3; # always send the first few for better visibility of the issue
 
     # failing job is re-tried every half hour, try to send one mail after 1, 2, 4, 8, etc. days