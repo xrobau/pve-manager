xrobau/pve-manager

commit eb1a09647d25d6d588260a971187a8803453a9ed

ui: vm network: allow to override MTU for virtio devices
we already have the 'mtu' option in the API, so we can just expose
that option inside the 'Advanced' menu for virtio network interfaces.

Signed-off-by: Oguz Bektas <o.bektas@proxmox.com>

Moved the message tooltip below the advanced columns to avoid the line
break and make it more visually pleasing.

Signed-off-by: Daniel Tschlatscher <d.tschlatscher@proxmox.com>

www/manager6/qemu/NetworkEdit.js
@@ -19,6 +19,7 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 	me.network.macaddr = values.macaddr;
 	me.network.disconnect = values.disconnect;
 	me.network.queues = values.queues;
+	me.network.mtu = values.mtu;
 
 	if (values.rate) {
 	    me.network.rate = values.rate;
@@ -31,6 +32,17 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 	params[me.confid] = PVE.Parser.printQemuNetwork(me.network);
 
 	return params;
+    },
+
+    viewModel: {
+	data: {
+	    networkModel: '',
+	    mtu: '',
+	},
+	formulas: {
+	    isVirtio: get => get('networkModel') === 'virtio',
+	    showMtuHint: get => get('mtu') === 1,
+	},
     },
 
     setNetwork: function(confid, data) {
@@ -93,6 +105,18 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 		fieldLabel: gettext('Disconnect'),
 		name: 'disconnect',
 	    },
+	    {
+		xtype: 'proxmoxintegerfield',
+		name: 'mtu',
+		fieldLabel: 'MTU',
+		bind: {
+		    disabled: '{!isVirtio}',
+		    value: '{mtu}',
+		},
+		minValue: 1,
+		maxValue: 65520,
+		allowBlank: true,
+	    },
 	];
 
 	if (me.insideWizard) {
@@ -112,6 +136,7 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 			    'macaddr',
 			    'rate',
 			    'queues',
+			    'mtu',
 			];
 			fields.forEach(function(fieldname) {
 			    me.down('field[name='+fieldname+']').setDisabled(value);
@@ -130,6 +155,7 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 		xtype: 'pveNetworkCardSelector',
 		name: 'model',
 		fieldLabel: gettext('Model'),
+		bind: '{networkModel}',
 		value: PVE.qemu.OSDefaults.generic.networkCard,
 		allowBlank: false,
 	    },
@@ -160,6 +186,16 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 		maxValue: 8,
 		value: '',
 		allowBlank: true,
+	    },
+	];
+	me.advancedColumnB = [
+	    {
+		xtype: 'displayfield',
+		userCls: 'pmx-hint',
+		value: gettext("Use the special value '1' to inherit the MTU value from the underlying bridge"),
+		bind: {
+		    hidden: '{!showMtuHint}',
+		},
 	    },
 	];
 

commit bd413ca078927e9bc7ac331b86c2f927221fb82a

ui: vm network edit: use the correct default model
commit eb1a09647d25d6d588260a971187a8803453a9ed broke the preselected
Model by setting it to ''. The GUI displays/selects an empty field
as default.

Fixes: eb1a0964 ("ui: vm network: allow to override MTU for virtio devices")
Signed-off-by: Matthias Heiserer <m.heiserer@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/qemu/NetworkEdit.js
@@ -36,7 +36,7 @@ Ext.define('PVE.qemu.NetworkInputPanel', {
 
     viewModel: {
 	data: {
-	    networkModel: '',
+	    networkModel: undefined,
 	    mtu: '',
 	},
 	formulas: {